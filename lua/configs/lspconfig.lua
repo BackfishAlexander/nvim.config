027e37b995de40b45841a455371357fa2dd2d361
alex <backfish.alexander@gmail.com>
2025-03-21 01:27:53 -0400

    New config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -7,6 +7,15 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+
+lspconfig.ts_ls.setup({
+  on_attach = function(client, bufnr)
+    -- Optionally disable formatting if using a separate formatter like null-ls
+    client.server_capabilities.documentFormattingProvider = false
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,24 @@
 require "nvchad.mappings"
 
--- add yours here
+vim.opt.relativenumber = true
+
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+
+
+
+-- Open floating diagnostics window
+map("n", "<leader>ce", vim.diagnostic.open_float, { desc = "Open floating diagnostics" })
+
+-- Go to previous diagnostic
+map("n", "<leader>cj", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+
+-- Go to next diagnostic
+map("n", "<leader>ck", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
