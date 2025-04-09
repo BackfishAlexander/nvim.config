2f400b33fd793f0c26485ffe032a88129bbd4357
Alex Backfish <backfish.alexander@gmail.com>
2025-04-09 15:58:11 -0400

    Added treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,15 +15,15 @@ return {
 
   {
     "ThePrimeagen/vim-be-good"
-  }
+  },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "typescript"
+  		},
+  	},
+  },
 }
